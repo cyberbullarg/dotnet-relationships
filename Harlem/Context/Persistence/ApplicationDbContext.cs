0453abfd775e3f6e6dabf3b15d588ea575a62e12
cyberbullarg <juan.lavallen@C11-VFOMD4548QK>
2024-01-11 12:03:41 -0300

    feat: relationships between entities performed

--- Harlem/Context/Persistence/ApplicationDbContext.cs
+++ Harlem/Context/Persistence/ApplicationDbContext.cs
@@ -8,5 +8,20 @@ namespace APIService.Context.Persistence
         public DbSet<Character> Characters { get; set; }
         public DbSet<Backpack> Backpacks { get; set; }
         public DbSet<Weapon> Weapons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Relationship between character and backpack
+            builder.Entity<Character>()
+                   .HasOne(c => c.Backpack)
+                   .WithOne(b => b.Character)
+                   .HasForeignKey<Backpack>(b => b.CharacterId);
+
+            // Relationship between character and weapon
+            builder.Entity<Character>()
+                   .HasMany(c => c.Weapons)
+                   .WithOne(w => w.Character)
+                   .HasForeignKey(w => w.CharacterId);
+        }
     }
 }
