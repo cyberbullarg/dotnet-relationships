81c98e2aafe0386365399c3cc4330d7770602069
cyberbullarg <juan.lavallen@C11-VFOMD4548QK>
2024-01-11 12:02:06 -0300

    fix: jsonignore attribute added

--- Harlem/Domain/Entities/Backpack.cs
+++ Harlem/Domain/Entities/Backpack.cs
@@ -1,10 +1,13 @@
-﻿namespace APIService.Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace APIService.Domain.Entities
 {
     public class Backpack
     {
         public Guid Id { get; set; }
-        public string Description { get; set; 
+        public string Description { get; set; }
         public Guid CharacterId { get; set; }
+        [JsonIgnore]
         public Character Character { get; set; }
     }
 }
--- Harlem/Domain/Entities/Weapon.cs
+++ Harlem/Domain/Entities/Weapon.cs
@@ -1,10 +1,13 @@
-﻿namespace APIService.Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace APIService.Domain.Entities
 {
     public class Weapon
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public Guid CharacterId { get; set; }
+        [JsonIgnore]
         public Character Character { get; set; }
     }
 }
